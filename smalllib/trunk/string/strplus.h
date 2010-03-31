d374fdef08294c51eb7f40863f8a2307840c8f99
sullivanz.pku <sullivanz.pku@f8d17060-f1c5-0fde-22b4-9c9e9bb934a5>
2010-03-31 05:57:30 +0000

    -----[temp]tried to add some string lower and string upper class. Unable to finish.

--- smalllib/trunk/string/strplus.h
+++ smalllib/trunk/string/strplus.h
@@ -1,6 +1,42 @@
-#ifndef STRPLUS_H
-#define STRPLUS_H
+
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <locale>
+
+
+#ifndef STRPLUS_H
+#define STRPLUS_H
+
+
 char* strcat_noOW(char *tostr, int tostr_size,char *str1, char *str2);
+
+/*
+std::string stoupper(std::string s)
+{ 
+  std::locale loc;
+  std::string::iterator i = s.begin();
+  std::string::iterator end = s.end();
+
+  while (i != end) {
+	  *i = std::toupper((unsigned char)*i,loc);
+    ++i;
+  }
+  return s;
+}
+
+std::string stolower(std::string s)
+{
+  std::locale loc;
+  std::string::iterator i = s.begin();
+  std::string::iterator end = s.end();
+
+  while (i != end) {
+	  *i = std::tolower((unsigned char)*i,loc);
+    ++i;
+  }
+  return s;
+}
+*/
+
 #endif /* STRPLUS_H */
